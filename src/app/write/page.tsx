15649c3162105869a3f547f30733b2a6bc677e8f
82orez <82orez@naver.com>
2025-01-08 09:32:29 +0900

    Input, Button

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,7 +2,7 @@ import WritePage from "@/components/WritePage";
 
 export default function Page() {
   return (
-    <div className="flex flex-col px-1 pb-4 pt-6 lg:px-3">
+    <div className="flex flex-col px-1 pt-6 lg:px-3">
       <h1 className="mb-4 text-2xl font-medium">새로운 글</h1>
 
       <WritePage />
