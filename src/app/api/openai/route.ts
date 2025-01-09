90236dd6c5fd1416ac402661f768e0bb731aa97b
82orez <82orez@naver.com>
2025-01-09 15:28:56 +0900

    openai-6

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -5,6 +5,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// * GET 요청으로 처리하는 부분.
 export async function GET(request: Request) {
   const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
@@ -13,6 +14,28 @@ export async function GET(request: Request) {
       {
         role: "user",
         content: "피자 만드는 법 알려줘.",
+      },
+    ],
+  });
+
+  // * completion.choices[0].message 부분은 openai api 의 공식. 객체(object)를 반환함.
+  console.log(completion.choices[0].message);
+
+  return NextResponse.json(completion.choices[0].message);
+}
+
+// * POST 요청으로 처리하는 부분
+// * 클라이언트 요청의 body 에 질문 내용을 받아와서 openai api 로 전달.
+export async function POST(request: Request) {
+  const { question } = await request.json();
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {
+        role: "user",
+        content: `${question}`,
       },
     ],
   });
